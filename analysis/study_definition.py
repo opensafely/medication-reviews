b3a8dfd568330158cdccf9b3557dd70b207d0bb6
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2022-08-08 12:59:13 +0100

    added age bands

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -32,6 +32,37 @@ study = StudyDefinition(
             "int": {"distribution": "population_ages"},
         },
     ),
+    age_band=patients.categorised_as(
+        {
+            "missing": "DEFAULT",
+            "0-19": """ age >= 0 AND age < 20""",
+            "20-29": """ age >=  20 AND age < 30""",
+            "30-39": """ age >=  30 AND age < 40""",
+            "40-49": """ age >=  40 AND age < 50""",
+            "50-59": """ age >=  50 AND age < 60""",
+            "60-69": """ age >=  60 AND age < 70""",
+            "70-79": """ age >=  70 AND age < 80""",
+            "80-89": """ age >=  80 AND age < 90""",
+            "90+": """ age >=  90 AND age <= 120""",
+        },
+        return_expectations={
+            "rate": "universal",
+            "category": {
+                "ratios": {
+                    "missing": 0.005,
+                    "0-19": 0.035,
+                    "20-29": 0.12,
+                    "30-39": 0.12,
+                    "40-49": 0.12,
+                    "50-59": 0.12,
+                    "60-69": 0.12,
+                    "70-79": 0.12,
+                    "80-89": 0.12,
+                    "80+": 0.12,
+                }
+            },
+        },
+    ),
     sex=patients.sex(
         return_expectations={
             "rate": "universal",
