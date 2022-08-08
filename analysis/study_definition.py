2ef488e002e6d4ff8b78023e2c227272aaf8c45b
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2022-08-08 18:08:07 +0100

    study pop: add care home

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -136,4 +136,28 @@ study = StudyDefinition(
             "incidence": 0.01,
         },
     ),
+    care_home_type=patients.care_home_status_as_of(
+        "last_day_of_month(index_date)",
+        categorised_as={
+            "PC":
+            """
+            IsPotentialCareHome
+            AND LocationDoesNotRequireNursing='Y'
+            AND LocationRequiresNursing='N'
+            """,
+            "PN":
+            """
+            IsPotentialCareHome
+            AND LocationDoesNotRequireNursing='N'
+            AND LocationRequiresNursing='Y'
+            """,
+            "PS": "IsPotentialCareHome",
+            "PR": "NOT IsPotentialCareHome",
+            "": "DEFAULT",
+        },
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"PC": 0.05, "PN": 0.05, "PS": 0.05, "PR": 0.84, "": 0.01},},
+        },
+    ),
 )
