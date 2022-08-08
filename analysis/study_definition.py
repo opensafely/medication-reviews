20e66ede65ebf54e57824c1968f6cec1507fd964
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2022-08-08 12:09:28 +0100

    define basic population

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -10,7 +10,19 @@ study = StudyDefinition(
         "rate": "uniform",
         "incidence": 0.5,
     },
-    population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
+    population=patients.satisfying(
+        """
+       registered AND
+       NOT died AND
+       (age >=18 AND age <=120) AND
+       (sex = 'M' OR sex = 'F')
+       """
+    ),
+    registered=patients.registered_as_of("index_date"),
+    # Or should this be registered with one practice?
+    died=patients.died_from_any_cause(
+        on_or_before="index_date",
+        returning="binary_flag",
+        return_expectations={"incidence": 0.1},
     ),
 )
