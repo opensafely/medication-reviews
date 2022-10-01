9abf9d7cd394b7b3f1487d72721de55a6e2d29c6
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2022-10-01 23:02:58 +0100

    added functionality

--- analysis/code_use_summary.py
+++ analysis/code_use_summary.py
@@ -4,7 +4,52 @@ import glob
 import pathlib
 from utilities import OUTPUT_DIR
 import numpy as np
-from codelists import *
+import os
+
+def create_codeuse_summary(paths):
+    df_codeuse = pd.DataFrame(columns=['Code','Uses','Date'])
+    codelistcolumns=[]
+    CodeColumn=[]
+    UsesColumn=[]
+    DateColumn=[]
+    for i, path in enumerate(paths):
+        if i==0:
+            filedate=get_date_from_filename(path)
+            df = pd.read_csv(path)
+            columnheaders=list(df)
+            for column in columnheaders:
+                if 'count_' in column:
+                    codelistcolumns.append(column)
+            for codecolumnname in codelistcolumns:
+                codeonly=codecolumnname.replace('count_','')
+                CodeColumn.append(codeonly)
+                UsesColumn.append(df[codecolumnname].sum())
+                DateColumn.append(filedate)
+        else:
+            filedate=get_date_from_filename(path)
+            df = pd.read_csv(path) 
+            for codecolumnname in codelistcolumns:
+                codeonly=codecolumnname.replace('count_','')
+                CodeColumn.append(codeonly)
+                UsesColumn.append(df[codecolumnname].sum())
+                DateColumn.append(filedate)
+    dict = {'Code':CodeColumn,
+            'Uses':UsesColumn,
+            'Date':DateColumn
+            }
+  
+    df_codeuse = pd.DataFrame(dict) 
+    return df_codeuse                        
+
+
+
+def get_date_from_filename(path):
+    pathsplit=os.path.normpath(path).split(os.path.sep)
+    filenamesplit=pathsplit[-1].split('.')
+    filename=filenamesplit[0].replace('input_', '')
+    filedatesplit=filename.split('-')
+    filedate=f'{filedatesplit[2]}/{filedatesplit[1]}/{filedatesplit[0]}'
+    return filedate
 
 def get_path(*args):
     return pathlib.Path(*args).resolve()
@@ -30,16 +75,8 @@ def parse_args():
 def main():
     args = parse_args()
     paths = args.study_def_paths
-    variables = {}
-    for code in code_list:
-        variables.update(make_variable(code))
-
-    #demographics = args.demographics.split(",")
-    #outcome = args.outcome
-
-    #table_1, had_outcome = create_table_1(paths, demographics, outcome)
-    #table_1.to_csv(OUTPUT_DIR / "table_1.csv")
-    #had_outcome.to_csv(OUTPUT_DIR / "table_1_had_outcome.csv")
+    df_codeuse=create_codeuse_summary(paths)
+    df_codeuse.to_csv(OUTPUT_DIR / "codeuse.csv")
 
 
 main()
