f80955351874cc55a5ea72c4e115c36a6acacf3c
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2022-10-04 20:13:37 +0100

    add function to match term to code

--- analysis/code_use_summary.py
+++ analysis/code_use_summary.py
@@ -2,40 +2,39 @@ import pandas as pd
 import argparse
 import glob
 import pathlib
-from utilities import OUTPUT_DIR, plot_measures
+from utilities import OUTPUT_DIR, CODELIST_DIR, plot_measures
 import numpy as np
 import os
 from pathlib import Path
+from codelists import med_review_codes
 
 if not (OUTPUT_DIR / "figures").exists():
     Path.mkdir(OUTPUT_DIR / "figures")
 
-def create_codeuse_summary(paths):
-    codelistcolumns=[]
+def get_column_codes(code_list):
+    variables = []
+    for code in code_list:
+        variables.append(code)
+    return variables
+
+def get_terms_for_codes(codelist_file):
+    df = pd.read_csv(CODELIST_DIR / codelist_file)
+    return df
+
+
+"codelists/user-chriswood-medication-review.csv"
+
+def create_codeuse_summary(paths, code_columns):
     CodeColumn=[]
     UsesColumn=[]
     DateColumn=[]
-    for i, path in enumerate(paths):
-        if i==0:
-            filedate=get_date_from_filename(path)
-            df = pd.read_csv(path)
-            columnheaders=list(df)
-            for column in columnheaders:
-                if 'count_' in column:
-                    codelistcolumns.append(column)
-            for codecolumnname in codelistcolumns:
-                codeonly=codecolumnname.replace('count_','')
-                CodeColumn.append(codeonly)
-                UsesColumn.append(df[codecolumnname].sum())
-                DateColumn.append(filedate)
-        else:
-            filedate=get_date_from_filename(path)
-            df = pd.read_csv(path) 
-            for codecolumnname in codelistcolumns:
-                codeonly=codecolumnname.replace('count_','')
-                CodeColumn.append(codeonly)
-                UsesColumn.append(df[codecolumnname].sum())
-                DateColumn.append(filedate)
+    for path in paths:
+        filedate=get_date_from_filename(path)
+        df = pd.read_csv(path) 
+        for codecolumnname in code_columns:
+            CodeColumn.append(codecolumnname)
+            UsesColumn.append(df[f'count_{codecolumnname}'].sum())
+            DateColumn.append(filedate)
     dict = {'code':CodeColumn,
             'uses':UsesColumn,
             'date':DateColumn
@@ -78,10 +77,15 @@ def parse_args():
 def main():
     args = parse_args()
     paths = args.study_def_paths
-    df_codeuse=create_codeuse_summary(paths)
+    code_columns = get_column_codes(med_review_codes)
+    df_codeuse=create_codeuse_summary(paths, code_columns)
+    df_codeuse['code']=df_codeuse['code'].astype(int)
+    terms_lookup=get_terms_for_codes()
+    df_codeuse = pd.merge(df_codeuse, terms_lookup, how='left', left_on = 'code', right_on = 'code')
+    df_codeuse=df_codeuse.reindex(columns=['code','term','uses','date'])
     df_codeuse.to_csv(OUTPUT_DIR / "codeuse.csv", index=False)
     df_codeuse['date']= pd.to_datetime(df_codeuse['date'])
-    plot_measures(df_codeuse, 'codeuse', '', 'uses', 'Uses of code',  category='code')
+    plot_measures(df_codeuse, 'codeuse', '', 'uses', 'Uses of code',  category='term')
 
 
 main()
