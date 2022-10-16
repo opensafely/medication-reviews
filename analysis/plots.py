8ab90de6b367b06c2b7b4f4217b9c7b71d70d0f0
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2022-10-16 21:18:12 +0100

    fix drop and round

--- analysis/plots.py
+++ analysis/plots.py
@@ -29,8 +29,8 @@ for med_review in med_review_type:
     df.to_csv(OUTPUT_DIR / f"redacted/redacted_measure_{med_review}_population_rate.csv", index=False,)
     for breakdownby in breakdowns:
         df = pd.read_csv(OUTPUT_DIR / f"joined/measure_{med_review}_{breakdownby}_rate.csv", parse_dates=["date"])
-        df = drop_and_round(f'had_{med_review}', base=5, threshold=7)
-        df = drop_and_round('population', base=5, threshold=7)
+        df[f'had_{med_review}'] = drop_and_round(df[f'had_{med_review}'], base=5, threshold=7)
+        df['population'] = drop_and_round(df['population'], base=5, threshold=7)
         df.to_csv(OUTPUT_DIR / f"redacted/redacted_measure_{med_review}_population_rate.csv", index=False,)
 
 for med_review in med_review_type:
