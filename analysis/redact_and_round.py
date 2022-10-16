d0b55015306ccde77ad170297f7667dd1d141052
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2022-10-16 22:19:47 +0100

    recalculate % after redaction

--- analysis/redact_and_round.py
+++ analysis/redact_and_round.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from redaction_utils import *
-from plots import breakdowns, med_review_type
+from pathlib import Path
+from plots import OUTPUT_DIR, breakdowns, med_review_type
 
 if not (OUTPUT_DIR / "redacted").exists():
     Path.mkdir(OUTPUT_DIR / "redacted")
@@ -10,9 +11,11 @@ for med_review in med_review_type:
     df = pd.read_csv(OUTPUT_DIR / f"joined/measure_{med_review}_population_rate.csv", parse_dates=["date"])
     df[f'had_{med_review}'] = drop_and_round(df[f'had_{med_review}'], base=5, threshold=7)
     df['population'] = drop_and_round(df['population'], base=5, threshold=7)
+    df['value'] = df[f'had_{med_review}']/df['population']
     df.to_csv(OUTPUT_DIR / f"redacted/redacted_measure_{med_review}_population_rate.csv", index=False,)
     for breakdownby in breakdowns:
         df = pd.read_csv(OUTPUT_DIR / f"joined/measure_{med_review}_{breakdownby}_rate.csv", parse_dates=["date"])
         df[f'had_{med_review}'] = drop_and_round(df[f'had_{med_review}'], base=5, threshold=7)
         df['population'] = drop_and_round(df['population'], base=5, threshold=7)
-        df.to_csv(OUTPUT_DIR / f"redacted/redacted_measure_{med_review}_population_rate.csv", index=False,)
+        df['value'] = df[f'had_{med_review}']/df['population']
+        df.to_csv(OUTPUT_DIR / f"redacted/redacted_measure_{med_review}_{breakdownby}_rate.csv", index=False,)
