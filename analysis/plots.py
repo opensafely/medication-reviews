dda6f86c80c49978aac9502b35fa3d62c60aedfd
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2022-10-18 14:24:43 +0100

    updated y-axis to smr/mr

--- analysis/plots.py
+++ analysis/plots.py
@@ -18,12 +18,17 @@ breakdowns=[
 
 med_review_type=["smr", "mr"]
 
+med_review_dict={
+    "smr" : "structured medication review",
+    "mr" : "medication review"
+}
+
 for med_review in med_review_type:
     df = pd.read_csv(OUTPUT_DIR / f"joined/measure_{med_review}_population_rate.csv", parse_dates=["date"])
     #Add column for rate per 1000 patients
     calculate_rate(df, f'had_{med_review}', 'population', rate_per=1000, round_rate=False)
     #Plot
-    plot_measures(df, filename=f"{med_review}_population_rate", title="", column_to_plot="rate", y_label="People who received a medication review per 1000 registered patients")
+    plot_measures(df, filename=f"{med_review}_population_rate", title="", column_to_plot="rate", y_label=f"People who received a {med_review_dict[med_review]} per 1000 registered patients")
     for breakdownby in breakdowns:
         df = pd.read_csv(OUTPUT_DIR / f"joined/measure_{med_review}_{breakdownby}_rate.csv", parse_dates=["date"])
         df[breakdownby] = df[breakdownby].fillna('missing')
@@ -36,4 +41,4 @@ for med_review in med_review_type:
             convert_binary(df, 'nhome', 'Record of individual living at a nursing home', 'No record of individual living at a nursing home')
         #Add column for rate per 1000 patients
         calculate_rate(df, f'had_{med_review}', 'population', rate_per=1000, round_rate=False)
-        plot_measures(df, filename=f"{med_review}_{breakdownby}_rate", title="", column_to_plot="rate", y_label="People who received a medication review per 1000 registered patients", category=breakdownby)
+        plot_measures(df, filename=f"{med_review}_{breakdownby}_rate", title="", column_to_plot="rate", y_label=f"People who received a {med_review_dict[med_review]} per 1000 registered patients", category=breakdownby)
