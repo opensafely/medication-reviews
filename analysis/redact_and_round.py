eb93b0ed0f44f98a3b7601b6edf125ec98bb994a
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2022-11-15 14:27:12 +0000

    redact and round allmedrev measures

--- analysis/redact_and_round.py
+++ analysis/redact_and_round.py
@@ -17,27 +17,26 @@ breakdowns=[
 "care_home_type"
 ]
 
-med_review_type=["smr", "mr"]
+med_review_type=["smr", "mr", "allmedrv"]
 
 #Redact measures files - Redact counts <=7 then round counts to nearest 5
 for med_review in med_review_type:
+    if (med_review=="allmedrv"):
+        numerator_col="had_anymedrev" # fix as column title doesn't match filename for allmedrv
+    else:
+        numerator_col=f'had_{med_review}'
     df = pd.read_csv(OUTPUT_DIR / f"joined/measure_{med_review}_population_rate.csv", parse_dates=["date"])
     if (med_review=="smr"):
         df = df.loc[(df['date'] >= '2020-01-01')] #Filter to only include dates inc and after Jan 2020
-    
-    df = redact_small_numbers(df, n=7, rounding_base=5, numerator=f'had_{med_review}', denominator="population", rate_column="value", date_column="date")  
-   
+    df = redact_small_numbers(df, n=7, rounding_base=5, numerator=numerator_col, denominator="population", rate_column="value", date_column="date")  
     df.to_csv(OUTPUT_DIR / f"redacted/redacted_measure_{med_review}_population_rate.csv", index=False,)
+
     for breakdownby in breakdowns:
         df = pd.read_csv(OUTPUT_DIR / f"joined/measure_{med_review}_{breakdownby}_rate.csv", parse_dates=["date"])
         if (med_review=="smr"):
             df = df.loc[(df['date'] >= '2020-01-01')] #Filter to only include dates inc and after Jan 2020
-        
-        df = redact_small_numbers(df, n=7, rounding_base=5, numerator=f'had_{med_review}', denominator="population", rate_column="value", date_column="date")  
-   
- 
+        df = redact_small_numbers(df, n=7, rounding_base=5, numerator=numerator_col, denominator="population", rate_column="value", date_column="date")  
         df.to_csv(OUTPUT_DIR / f"redacted/redacted_measure_{med_review}_{breakdownby}_rate.csv", index=False,)
-
 
 #Redact total codeuse counts files - Redact counts >0 and <=7 then round counts to nearest 5
 codeusefiles=["totalcodeuse", "totalcodeuse_allmedrev"]
