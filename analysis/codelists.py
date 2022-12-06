167c9960980128f4d54656c223c10e546a2c8e61
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2022-12-06 16:20:06 +0000

    update codelists

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -44,11 +44,23 @@ allmed_review_codes = codelist_from_csv(
 highdoseopioid_codes = codelist_from_csv(
     "codelists/opensafely-high-dose-long-acting-opioids-openprescribing-dmd.csv",
     system="snomed",
-    column="code",
+    column="dmd_id",
 )
 
 dmard_codes = codelist_from_csv(
     "codelists/opensafely-dmards.csv",
     system="snomed",
-    column="code",
+    column="snomed_id",
+)
+
+teratogenic_codes = codelist_from_csv(
+    "codelists/opensafely-teratogenic-medicines.csv",
+    system="snomed",
+    column="dmd_id",
+)
+
+addictivemeds_codes = codelist_from_csv(
+    "codelists/opensafely-addictive-medicines.csv",
+    system="snomed",
+    column="dmd_id",
 )
