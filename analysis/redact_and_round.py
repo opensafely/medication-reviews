d63991b64ae80ad23f3885a28277e4d16b9d61c0
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2022-12-11 19:06:15 +0000

    Add 12m measures to be retracted

--- analysis/redact_and_round.py
+++ analysis/redact_and_round.py
@@ -17,23 +17,25 @@ breakdowns=[
 "care_home_type"
 ]
 
-med_review_type=["smr", "mr", "allmedrv"]
+med_review_type=["smr", "smr12m", "mr", "mr12m", "allmedrv", "allmedrv12m"]
 
 #Redact measures files - Redact counts <=7 then round counts to nearest 5
 for med_review in med_review_type:
     if (med_review=="allmedrv"):
         numerator_col="had_anymedrev" # fix as column title doesn't match filename for allmedrv
+    elif (med_review=="allmedrv12m"):
+        numerator_col="had_anymedrev12m" # fix as column title doesn't match filename for allmedrv
     else:
         numerator_col=f'had_{med_review}'
     df = pd.read_csv(OUTPUT_DIR / f"joined/measure_{med_review}_population_rate.csv", parse_dates=["date"])
-    if (med_review=="smr"):
+    if (med_review=="smr" or med_review=="smr12m"):
         df = df.loc[(df['date'] >= '2020-01-01')] #Filter to only include dates inc and after Jan 2020
     df = redact_small_numbers(df, n=7, rounding_base=5, numerator=numerator_col, denominator="population", rate_column="value", date_column="date")  
     df.to_csv(OUTPUT_DIR / f"redacted/redacted_measure_{med_review}_population_rate.csv", index=False,)
 
     for breakdownby in breakdowns:
         df = pd.read_csv(OUTPUT_DIR / f"joined/measure_{med_review}_{breakdownby}_rate.csv", parse_dates=["date"])
-        if (med_review=="smr"):
+        if (med_review=="smr" or med_review=="smr12m"):
             df = df.loc[(df['date'] >= '2020-01-01')] #Filter to only include dates inc and after Jan 2020
         df = redact_small_numbers(df, n=7, rounding_base=5, numerator=numerator_col, denominator="population", rate_column="value", date_column="date")  
         df.to_csv(OUTPUT_DIR / f"redacted/redacted_measure_{med_review}_{breakdownby}_rate.csv", index=False,)
