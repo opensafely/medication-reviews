84ff2f95b91100cbe53325f5cb3b57716204d7da
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-12-13 16:39:29 +0000

    refactor table_1.py This splits out the main function into multiple smaller functions so we can test them

--- analysis/table_1.py
+++ analysis/table_1.py
@@ -2,43 +2,73 @@ import pandas as pd
 import argparse
 import glob
 import pathlib
-from utilities import OUTPUT_DIR
+from analysis.utilities import OUTPUT_DIR
 import numpy as np
 
+def get_path(*args):
+    return pathlib.Path(*args).resolve()
 
-def create_table_1(paths, demographics, outcome):
+def match_paths(pattern):
+    return [get_path(x) for x in sorted(glob.glob(pattern))]
 
-    for i, path in enumerate(paths):
-        if i ==0:
-            df = pd.read_csv(path, usecols=demographics + ["patient_id", outcome])
-            #fill in missing values
-            df.loc[:,demographics] = df.loc[:,demographics].fillna("missing")
-            df_had_outcome = df.loc[df[outcome]==1,:]
-            df = df.drop(outcome, axis=1)
-            df_had_outcome = df_had_outcome.drop(outcome, axis=1)
-           
-        
-        else:
-            updated_df = pd.read_csv(path, usecols=demographics + ["patient_id", outcome])
-            updated_df.loc[:,demographics] = updated_df.loc[:,demographics].fillna("missing")
-            updated_df_had_outcome = updated_df.loc[updated_df[outcome]==1,:]
-            updated_df = updated_df.drop(outcome, axis=1)
-            updated_df_had_outcome = updated_df_had_outcome.drop(outcome, axis=1)
+def fill_missing_values(df, columns):
+    """
+    Fill missing values in the dataframe in provided columns with 'missing'.
+    Args:
+        df (pd.DataFrame): Dataframe to fill
+        columns (list): List of columns to fill
+    Returns:
+        df (pd.DataFrame): Dataframe with missing values filled
+    """    
+
+    for column in columns:
+        df[column] = df[column].fillna('missing')
+
+    return df
+
+def subset_outcome_of_interest(df, outcome):
+    """
+    Subset the dataframe to only include patients with the outcome of interest.
+    Args:
+        df (pd.DataFrame): Dataframe to subset
+        outcome (str): Outcome of interest
+    Returns:
+        df (pd.DataFrame): Dataframe with the outcome of interest removed
+        df_had_outcome (pd.DataFrame): Dataframe with only patients with the outcome of interest
+    """
+    df_had_outcome = df.loc[df[outcome]==1,:].reset_index(drop=True)
+    df_had_outcome = df_had_outcome
+    df = df.drop(outcome, axis=1)
+    df_had_outcome = df_had_outcome.drop(outcome, axis=1)
+    return df, df_had_outcome
 
 
-            
-            df = update_df(df, updated_df, columns=demographics)
-            df_had_outcome = update_df(df_had_outcome, updated_df_had_outcome, columns=demographics)
+def get_counts(df):
+    """
+    Get counts of patients in the dataframe for each column (demographic).
+    Args:
+        df (pd.DataFrame): Dataframe to get counts for
+    Returns:
+        counts (pd.DataFrame): Dataframe with counts for each demographic
+    """
     
     df = df.drop("patient_id", axis=1)
     df_counts = df.apply(lambda x: x.value_counts()).T.stack()
-
-    df_had_outcome = df_had_outcome.drop("patient_id", axis=1)
-    df_counts_had_outcome = df_had_outcome.apply(lambda x: x.value_counts()).T.stack()
-
-    return df_counts, df_counts_had_outcome
+    # set name to count
+    df_counts.name = "count"
+    return df_counts
    
 def update_df(original_df, new_df, columns=[], on="patient_id"):
+    """
+    Update a dataframe with new data, overwriting old data where multiple values exist.
+    Args:
+        original_df (pd.DataFrame): Original dataframe
+        new_df (pd.DataFrame): New dataframe
+        columns (list): Columns to update
+        on (str): Column to merge on
+    Returns:
+        pd.DataFrame: Updated dataframe
+    """
     updated = original_df.merge(
         new_df, on=on, how="outer", suffixes=("_old", "_new"), indicator=True
     )
@@ -51,13 +81,51 @@ def update_df(original_df, new_df, columns=[], on="patient_id"):
     updated = updated.drop(["_merge"], axis=1)
     return updated
 
-def get_path(*args):
-    return pathlib.Path(*args).resolve()
 
 
-def match_paths(pattern):
-    return [get_path(x) for x in sorted(glob.glob(pattern))]
+def create_table_1(paths, demographics, outcome):
+    """
+    Create a table 1 from a list of paths to csv files for the entire population and those with the outcome
+    of interest. For each file in paths, the function will read in the demographics and outcome columns and
+    merge them with the previous files. If a patient has multiple entries, the most recent entry will be used.
+    Args:
+        paths (list): List of paths to csv files
+        demographics (list): List of demographics to include in the table
+        outcome (str): Outcome of interest
+    Returns:
+        pd.DataFrame: Table 1
+    """
 
+    for i, path in enumerate(paths):
+        if i ==0:
+            # for the first file in the provided paths, we load it as the dataframe
+            df = pd.read_csv(path, usecols=demographics + ["patient_id", outcome])
+            
+            #fill in missing values
+            df = fill_missing_values(df, demographics)
+
+            # create subset of patients with outcome of interest
+            df, df_had_outcome = subset_outcome_of_interest(df, outcome)
+            
+        
+        else:
+            # if not the first file in the provided paths, we load it as the updated dataframe
+            updated_df = pd.read_csv(path, usecols=demographics + ["patient_id", outcome])
+
+            #fill in missing values
+            updated_df = fill_missing_values(updated_df, demographics)
+
+            # create subset of patients with outcome of interest
+            updated_df, updated_df_had_outcome = subset_outcome_of_interest(updated_df, outcome)
+
+            # update the original dataframe and with outcome dataframe
+            df = update_df(df, updated_df, columns=demographics)
+            df_had_outcome = update_df(df_had_outcome, updated_df_had_outcome, columns=demographics)
+    
+    df_counts = get_counts(df)
+    df_counts_had_outcome = get_counts(df_had_outcome)
+
+    return df_counts, df_counts_had_outcome
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -97,5 +165,5 @@ def main():
     table_1.to_csv(OUTPUT_DIR / "table_1.csv")
     had_outcome.to_csv(OUTPUT_DIR / "table_1_had_outcome.csv")
 
-
-main()
+if __name__ == "__main__":
+    main()
