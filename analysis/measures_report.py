2035b186577ef49e1f1a98c526b73654d01df2d2
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2023-01-04 15:50:55 +0000

    fix measures regex

--- analysis/measures_report.py
+++ analysis/measures_report.py
@@ -5,7 +5,7 @@ import re
 
 def match_input_files(file: str) -> bool:
     """Checks if file name has format outputted by cohort extractor"""
-    pattern = r"^input_20\d\d-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])\.csv.gz"
+    pattern = r"^input_report_20\d\d-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])\.csv.gz"
     return True if re.match(pattern, file) else False
 
 
@@ -16,7 +16,7 @@ def get_date_input_file(file: str) -> str:
         raise Exception("Not valid input file format")
 
     else:
-        date = re.search(r"input_(.*).csv.gz", file)
+        date = re.search(r"input_report_(.*).csv.gz", file)
         return date.group(1)
 
 def parse_args():
@@ -40,12 +40,15 @@ def main():
     for b in breakdowns:
         data[b] = []
 
+    print(breakdowns)
+
     for file in Path(args.input_dir).iterdir():
       
         if match_input_files(file.name):
             date = get_date_input_file(file.name)
             
             df = pd.read_csv(file)
+            print(df.columns)
             df["date"] = date
             count = df.loc[:,"event_measure"].sum()
             population = df.loc[:,"event_measure"].count()
