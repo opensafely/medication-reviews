5eff03c5d419f6ff8de9a50d6e8d35a3d835d395
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2023-01-30 12:26:15 +0000

    remove unneeded total code count section

--- analysis/standardised_redact_and_round.py
+++ analysis/standardised_redact_and_round.py
@@ -43,10 +43,3 @@ for med_review in med_review_type:
             df = df.loc[(df['date'] >= '2020-01-01')] #Filter to only include dates inc and after Jan 2020
         df = redact_small_numbers(df, n=7, rounding_base=5, numerator=numerator_col, denominator="population", rate_column="value", date_column="date")  
         df.to_csv(OUTPUT_DIR / f"redacted/redacted_measure_{med_review}_{breakdownby}_rate.csv", index=False,)
-
-#Redact total codeuse counts files - Redact counts >0 and <=7 then round counts to nearest 5
-codeusefiles=["totalcodeuse", "totalcodeuse_allmedrev"]
-for file in codeusefiles:
-    df = pd.read_csv(OUTPUT_DIR / f"{file}.csv")
-    df = codeuse_redact_small_numbers(df, n=7, rounding_base=5, column="uses")
-    df.to_csv(OUTPUT_DIR / f"redacted/standardised/redacted_{file}.csv", index=False,)
