8079f27106807f38d667e33a12a0ce791a03b555
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2023-01-30 12:30:21 +0000

    updated output csv filepath

--- analysis/standardised_redact_and_round.py
+++ analysis/standardised_redact_and_round.py
@@ -33,9 +33,9 @@ for med_review in med_review_type:
         numerator_col=f'had_{med_review}'
     df = pd.read_csv(OUTPUT_DIR / f"correctedagegroupsmeasures/{med_review}_population_rate_agesexstandardgrouped_corrected_standardised.csv", parse_dates=["date"])
     df = redact_small_numbers(df, n=7, rounding_base=5, numerator=numerator_col, denominator="population", rate_column="value", date_column="date")  
-    df.to_csv(OUTPUT_DIR / f"redacted/redacted_measure_{med_review}_population_rate.csv", index=False,)
+    df.to_csv(OUTPUT_DIR / f"redacted/standardised/redacted_standardised_measure_{med_review}_population_rate.csv", index=False,)
 
     for breakdownby in breakdowns:
         df = pd.read_csv(OUTPUT_DIR / f"correctedagegroupsmeasures/{med_review}_{breakdownby}_rate_agesexstandardgrouped_corrected_standardised.csv", parse_dates=["date"])
         df = redact_small_numbers(df, n=7, rounding_base=5, numerator=numerator_col, denominator="population", rate_column="value", date_column="date")  
-        df.to_csv(OUTPUT_DIR / f"redacted/redacted_measure_{med_review}_{breakdownby}_rate.csv", index=False,)
+        df.to_csv(OUTPUT_DIR / f"redacted/standardised/redacted_standardised_measure_{med_review}_{breakdownby}_rate.csv", index=False,)
