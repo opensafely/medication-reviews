0fdddc5e871a19a7873b04e83cc84f16d13b55df
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2023-06-13 15:28:07 +0100

    Added show SMR line variable

--- analysis/standardised-plots.py
+++ analysis/standardised-plots.py
@@ -42,10 +42,16 @@ for med_review in med_review_type:
         numerator_col="had_anymedrev12m" # fix as column title doesn't match filename for allmedrv
     else:
         numerator_col=f'had_{med_review}'
+
+    if (med_review=="smr" or med_review=="smr12m"):
+        smrstartshow=True
+    else:
+        smrstartshow=False
+
     df = pd.read_csv(OUTPUT_DIR / f"redacted-standardised/redacted_standardised_measure_{med_review}_population_rate.csv", parse_dates=["date"])
     df['percentrate']=df['UK Standard population rate per 100,000']/1000
 
-    plot_measures(df, filename=f"{med_review}_population_rate_standardised", title="", column_to_plot='percentrate', y_label=f"Percentage of people who received a {med_review_dict[med_review]} ", outputfilepath="figures-standardised") #Plot
+    plot_measures(df, filename=f"{med_review}_population_rate_standardised", title="", column_to_plot='percentrate', y_label=f"Percentage of people who received a \n{med_review_dict[med_review]} ", outputfilepath="figures-standardised", smrstart=smrstartshow) #Plot
 
     for breakdownby in breakdowns:
         df = pd.read_csv(OUTPUT_DIR / f"redacted-standardised/redacted_standardised_measure_{med_review}_{breakdownby}_rate.csv", parse_dates=["date"])
@@ -53,11 +59,11 @@ for med_review in med_review_type:
         breakdownbycol=columnlookupdict.get(breakdownby, breakdownby)
         df[breakdownbycol] = df[breakdownbycol].fillna('missing')
         if (breakdownby == "care_home_type"): 
-            convert_binary(df, 'care_home_type', 'Record of individual living \nat a care/nursing home', 'No record of individual living \nat a care/nursing home')
+            convert_binary(df, 'care_home_type', 'Record of individual living at a care/nursing home', 'No record of individual living at a care/nursing home')
         if (breakdownby == "learning_disability"):
             convert_binary(df, 'learning_disability', 'Record of learning disability', 'No record of learning disability')
         if (breakdownby == "nhome"):
-            convert_binary(df, 'nhome', 'Record of individual living \nat a care/nursing home', 'No record of individual living \nat a care/nursing home')
+            convert_binary(df, 'nhome', 'Record of individual living at a care/nursing home', 'No record of individual living at a care/nursing home')
         if (breakdownbycol == "addictivemeds_last12m"):
             convert_binary(df, 'addictivemeds_last12m', 'Record of prescription for an addictive medicine', 'No record of prescription for an addictive medicine')
         if (breakdownbycol == "dmards_last12m"):
@@ -69,4 +75,4 @@ for med_review in med_review_type:
         if (breakdownby == "sex"):
             df = relabel_sex(df)
         #Add column for rate per 1000 patients
-        plot_measures(df, filename=f"{med_review}_{breakdownby}_rate_standardised", title="", column_to_plot="percentrate", y_label=f"Percentage of people who received a {med_review_dict[med_review]}", category=breakdownbycol, outputfilepath="figures-standardised")
+        plot_measures(df, filename=f"{med_review}_{breakdownby}_rate_standardised", title="", column_to_plot="percentrate", y_label=f"Percentage of people who received a \n{med_review_dict[med_review]}", category=breakdownbycol, outputfilepath="figures-standardised", smrstart=smrstartshow)
